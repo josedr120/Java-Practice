d49b8547692ab78a9f89e094738f2a29e82963e9
JoseDR120 <josedrgerena120@gmail.com>
2019-12-13 21:02:42 -0400

    Initial Commit

--- Task2/Program2.java
+++ Task2/Program2.java
@@ -7,9 +7,26 @@ public class Program2 {
         Scanner x = new Scanner(System.in);
 
         int num;
+        int flag = 0;
 
-        System.out.print("Enter Number: "); num = x.nextInt();
 
+        System.out.print("Enter Number: ");
+        num = x.nextInt();
 
+        if (num == 0 || num == 1) {
+            System.out.println(num + " is not prime number");
+        } else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    System.out.println(num + " is not prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 0) {
+                System.out.println(num + " is prime number");
+            }
+        }
     }
 }
---
+++ Task2/Program4.java
@@ -0,0 +1,7 @@
+package Task2;
+
+public class Program4 {
+    public static void main(String[] args) {
+
+    }
+}
